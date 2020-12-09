48cd382e45f0c231855239d7c427359bcfe299e2
Lisa Kim <lisankim3@gmail.com>
2020-12-09 10:05:41 -0500

    exercise filter beginning

--- final-capstone/vue/src/views/Exercises.vue
+++ final-capstone/vue/src/views/Exercises.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
     <h1>Manage Exercises</h1>
+    <v-combobox
+      id="focus"
+      outlined
+      class="form-control"
+      :rules="focusRules"
+      label="Focus"
+      :items="focusNames"
+      v-model="pickedFocusName"
+      required
+    />
+    <v-text-field
+      id="time"
+      suffix="seconds"
+      outlined
+      label="Time"
+      v-model.number="exercise.time"
+      required
+      class="form-control"
+      :rules="requiredNumericRules"
+      append-outer-icon="mdi-chevron-up"
+      prepend-icon="mdi-chevron-down"
+      @click:append-outer="exercise.time += 10"
+      @click:prepend="exercise.time -= 10"
+    />
     <exercise-list />
   </div>
 </template>
