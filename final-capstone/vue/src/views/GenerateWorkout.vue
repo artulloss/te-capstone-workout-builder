6977c459faf9bd54edc610a4106aee5e9e1c488c
Adam Tulloss <artulloss@gmail.com>
2020-12-13 20:59:24 -0500

    Generate workout works

--- final-capstone/vue/src/views/GenerateWorkout.vue
+++ final-capstone/vue/src/views/GenerateWorkout.vue
@@ -74,38 +74,11 @@ export default {
   data() {
     return {
       exercises: [],
-      exercisesTest: [
-        {
-          exerciseId: 3,
-          exerciseName: "Soccer Drill Toe Taps",
-          focusId: 1,
-          description:
-            "Place a ball in the front of you, keep your hips forward, lifting up your knees, tap the top of the ball with the ball of your foot, then jump switch and tap the ball with the opposite foot.",
-          weight: null,
-          time: 30,
-          repetitions: null,
-          sets: null,
-          userId: 2,
-        },
-        {
-          exerciseId: 4,
-          exerciseName: "leap name",
-          focusId: 1,
-          description:
-            "Place a ball in the front of you, keep your hips forward, lifting up your knees, tap the top of the ball with the ball of your foot, then jump switch and tap the ball with the opposite foot.",
-          weight: null,
-          time: 30,
-          repetitions: null,
-          sets: null,
-          userId: 2,
-        },
-      ],
       selectedTrainers: [],
       selectedFocuses: [],
       selectedTime: null,
       trainers: [],
       focuses: [],
-
       numericRules: [(v) => (v || 0) >= 0 || "Negative values are not allowed"],
     };
   },
@@ -167,47 +140,43 @@ export default {
       this.selectedTime = newTime === 0 ? "" : newTime;
       //this.updateUrl(); // TODO Maybe
     },
-    onGenerate() {
-      this.getTrainerExercises().then((response) => {
-        if (response.status === 200) {
-          console.log("fix this");
-        }
-      });
+    async onGenerate() {
+      const exercises = await this.getTrainerExercises();
+      const workout = this.generateWorkout(exercises);
+      this.replaceWorkout(workout);
     },
-    replaceWorkout() {
+    replaceWorkout(exercises) {
       const username = this.$store.state.user.username;
       exerciseService.deleteUserExercises(username).then((response) => {
         if (response.status === 204) {
-          exerciseService
-            .addUserExercises(username, this.exercisesTest)
-            .then((r) => {
-              if (r.status === 201) {
-                this.$router.push({ name: "exercises" });
-              }
-            });
+          exerciseService.addUserExercises(username, exercises).then((r) => {
+            if (r.status === 201) {
+              this.$router.push({ name: "exercises" });
+            }
+          });
         }
       });
     },
-    getTrainerExercises() {
+    async getTrainerExercises() {
+      let exercises = [];
       for (const trainerName of this.selectedTrainers) {
-        exerciseService
-          .getTrainerExercises(trainerName)
-          .then((response) => {
-            console.log(response);
-            if (response.status === 200) {
-              this.exercises.concat(response.data);
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        const response = await exerciseService.getTrainerExercises(trainerName);
+        console.log({ response });
+        if (response.status === 200) {
+          console.log("DATA", response.data);
+          exercises = exercises.concat(response.data);
+          console.log("CONCATED", { exercises });
+        }
       }
+      return exercises;
     },
-    generateWorkout() {
+    generateWorkout(exercises) {
+      console.log(exercises);
       let time = 0;
       const workout = [];
-      while (time <= this.selectedTime) {
-        const exercise = this.exercises[randomNumber - 1];
+      while (time < this.selectedTime) {
+        const exercise = exercises[randomNumber(exercises.length) - 1];
+        console.log(exercise);
         time += exercise.time;
         workout.push(exercise);
       }
