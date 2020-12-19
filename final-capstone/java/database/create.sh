3571792689a68b79a33c14b16b7ebb75e23ad253
artulloss <artulloss@gmail.com>
2020-12-19 12:28:09 -0500

    Disgusting 😤

--- final-capstone/java/database/create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BASEDIR=$(dirname $0)
-DATABASE=final_capstone
-psql -U postgres -f "$BASEDIR/dropdb.sql" &&
-createdb -U postgres $DATABASE &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
